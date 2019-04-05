483fb0972b0ab48e3cef9f9be13720cb62e8c65d
Tirocigno <fede.naldini1@gmail.com>
2019-04-05 11:05:49 +0200

    Created Scala Files

---
+++ src/main/scala/streaming/exercise/exercise0.scala
@@ -0,0 +1,18 @@
+package streaming.exercise
+
+import org.apache.spark.SparkContext
+
+class exercise0 {
+
+  import org.apache.spark.streaming._
+   import org.apache.spark.storage.StorageLevel
+  val sc = new SparkContext();
+   val ssc = new StreamingContext(sc, Seconds(3))
+  val lines = ssc.socketTextStream("137.204.72.240",9852,
+    StorageLevel.MEMORY_AND_DISK_SER)
+   val words = lines.flatMap(_.split(" "))
+   val count = words.count()
+   count.print()
+   ssc.start()
+   ssc5.stop(false)
+}
